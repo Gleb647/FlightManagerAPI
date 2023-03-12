ff653ee04fb399d2eaca5b0086b283845bbc8644
gleb <glebtkach@rambler.ru>
2023-03-12 18:43:06 +0200

    added info about flight in TicketService

--- demo/src/main/java/com/example/demo/Service/TicketService.java
+++ demo/src/main/java/com/example/demo/Service/TicketService.java
@@ -1,13 +1,18 @@
 package com.example.demo.Service;
 
 import com.example.demo.EmailDetails.EmailDetails;
+import com.example.demo.Model.Flight;
 import com.example.demo.Model.FlightInfoEntity;
 import com.example.demo.Model.Ticket;
 import com.example.demo.Model.User;
 import com.example.demo.Repository.FlightInfoRepository;
+import com.example.demo.Repository.FlightsRepository;
 import com.example.demo.Repository.TicketRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @Service
 public class TicketService {
@@ -16,7 +21,7 @@ public class TicketService {
     public UserService user_service;
 
     @Autowired
-    public FlightInfoRepository flight_repo;
+    public FlightInfoRepository flight_info_repo;
 
     @Autowired
     public TicketRepo ticket_repo;
@@ -26,18 +31,23 @@ public class TicketService {
 
     public void buyTicket(Long id, String username){
         User user = user_service.getUser(username);
-        String name =user.getName();
-        FlightInfoEntity flight = flight_repo.findById(id).get();
-        Ticket ticket = new Ticket(user, flight);
+        String name = user.getName();
+        FlightInfoEntity flightinfo = flight_info_repo.findById(id).get();
+        Ticket ticket = new Ticket(user, flightinfo);
         ticket_repo.save(ticket);
+        Flight flight = flightinfo.getFlight();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy, HH:mm");
         EmailDetails details = new EmailDetails(
-                "glebtkach@rambler.ru",
-                "Dear "+ name,
+                        username,
                 "Info about flight:\n"+
-                        flight.getDate()+"\n"+
-                        flight.getCarrier()+"\n"+
-                        flight.getCost()+"\n"+
-                        flight.getFlightDuration()+"\n",
+                        "From: "+flight.getDeparture()+"\n"+
+                        "To: "+flight.getDestination()+"\n"+
+                        "Date: "+flightinfo.getDate().format(formatter)+"\n"+
+                        "Carrier: "+flightinfo.getCarrier()+"\n"+
+                        "Cost: "+flightinfo.getCost()+"$\n"+
+                        "Flight duration: "+flightinfo.getFlightDuration()+"h\n",
+                "Dear "+ name,
+
                 null);
         email_service.sendSimpleMail(details);
     }
